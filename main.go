2c4a6ca87afb54e8acd73b9dea123579e50b7370
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-04 18:51:03 +0900

    return hands

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func Rand() int {
+	n := rand.Intn(12)
+	return n + 1
+}
+
+func main() {
+	a, b := Rand(), Rand()
+	fmt.Println("あなたの手札は", a, "と", b, "です。")
 }
