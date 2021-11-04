750740a6a8589f5e68bf7a503a11e0124d554945
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-04 18:57:53 +0900

    Add input parts

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 func Rand() int {
@@ -10,7 +11,21 @@ func Rand() int {
 	return n + 1
 }
 
+func Valid(s string) bool {
+	return s == "hit" || s == "stay"
+}
+
 func main() {
 	a, b := Rand(), Rand()
+	var s string
+
+	// 対話部分
 	fmt.Println("あなたの手札は", a, "と", b, "です。")
+	fmt.Println("ヒットするならhit, ステイならstayと入力してください")
+	fmt.Scanf("%s", &s)
+	if Valid(s) == false {
+		fmt.Println("hitかstayと入力してください", s)
+		os.Exit(1)
+	}
+
 }
