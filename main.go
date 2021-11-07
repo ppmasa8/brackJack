3bad4baf3f0a5169f58763963736084fe326ffd6
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-07 23:19:57 +0900

    Add hit and stay func

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"os"
 )
 
+// playerの手札
+type player []int
+
+// CPUの手札
+type dealer []int
+
 func Rand() int {
 	n := rand.Intn(12)
 	return n + 1
@@ -15,25 +21,29 @@ func validHitOrStay(s string) bool {
 	return s == "hit" || s == "stay"
 }
 
-func Hit(p []int) {
+func Hit(p *player) []int {
 	c := Rand()
-	p = append(p, c)
-	for _, v := range p {
+	cp := *p
+	cp = append(cp, c)
+	for _, v := range cp {
 		fmt.Println(v)
 	}
+	*p = cp
+	return *p
 }
 
-const NoneBurst  = 0
-const PBurst     = 1
-const DBurst     = 2
+const NoneBurst = 0
+const PBurst = 1
+const DBurst = 2
 const PAndDBurst = 3
 
-func validHands(p, d []int) int {
+func validHands(p *player, d *dealer) int {
 	cntP, cntD := 0, 0
-	for _, v := range p {
+	cpP, cpD := *p, *d
+	for _, v := range cpP {
 		cntP += v
 	}
-	for _, v := range d {
+	for _, v := range cpD {
 		cntD += v
 	}
 
@@ -52,12 +62,13 @@ const PWin = 0
 const DWin = 1
 const Draw = 2
 
-func Judge(p, d []int) int {
+func Judge(p *player, d *dealer) int {
 	cntP, cntD := 0, 0
-	for _, v := range p {
+	cpP, cpD := *p, *d
+	for _, v := range cpP {
 		cntP += v
 	}
-	for _, v := range d {
+	for _, v := range cpD {
 		cntD += v
 	}
 
@@ -70,31 +81,36 @@ func Judge(p, d []int) int {
 	}
 }
 
-func main() {
+func hitOrStay(p *player) []int {
 	var s string
-
-	// プレイヤー
-	p := []int{}
-	// ディーラー
-	d := []int{}
-
-	fmt.Println("あなたの手札")
-	for i := 0; i < 2; i++ {
-		pn := Rand()
-		p = append(p, pn)
-		d = append(d, Rand())
-		fmt.Println(pn, " ")
-	}
+	cp := *p
 	fmt.Println("ヒットするならhit, ステイならstayと入力してください")
 	fmt.Scanf("%s", &s)
 	if validHitOrStay(s) == false {
 		fmt.Println("hitかstayと入力してください", s)
 		os.Exit(1)
 	}
+	if s == "hit" {
+		cp = Hit(p)
+		cp = hitOrStay(&cp)
+	} else {
+		return cp
+	}
+	return cp
+}
 
-	if s == "stay" {
-
+func main() {
+	cpP, cpD := player{}, dealer{}
+	fmt.Println("あなたの手札")
+	for i := 0; i < 2; i++ {
+		pn := Rand()
+		cpP = append(cpP, pn)
+		cpD = append(cpD, Rand())
+		fmt.Println(pn, " ")
 	}
 
+	// hitかstayを選ぶ
+	p := hitOrStay(&cpP)
 
+	fmt.Println(p)
 }
